d060ee39297975ffc5a0397dbb184a656191362e
lifei <876451336@qq.com>
2021-11-15 11:00:27 +0800

    完善列表组件抽离

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,6 +1,50 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { Table, Image, Spin, Tag } from 'ant-design-vue'
 import { useClientClouds } from '@/hooks/api/cloud.hooks'
+import { NodeCloud } from '@/types'
+
+const CloudProps = {
+	node: { type: Object as PropType<NodeCloud | null>, default: null }
+}
+
+const CreateElement = (text: string, color: string) => (
+	<Tag color={color} style={{ margin: 0 }}>
+		{text}
+	</Tag>
+)
+
+const NodeCover = defineComponent({
+	name: 'NodeCover',
+	props: CloudProps,
+	setup(props) {
+		return () => (
+			<Image
+				src={`${props.node?.cover}?x-oss-process=style/resize`}
+				style={{ width: '96px', height: '54px', cursor: 'pointer' }}
+			></Image>
+		)
+	}
+})
+
+const NodeType = defineComponent({
+	name: 'NodeType',
+	props: CloudProps,
+	setup(props) {
+		return () => {
+			return props.node?.type === 1 ? CreateElement('单集媒体', 'red') : CreateElement('多集媒体', 'cyan')
+		}
+	}
+})
+
+const NodeStatus = defineComponent({
+	name: 'NodeStatus',
+	props: CloudProps,
+	setup(props) {
+		return () => {
+			return props.node?.type === 1 ? CreateElement('启用', 'green') : CreateElement('禁用', 'pink')
+		}
+	}
+})
 
 export default defineComponent({
 	name: 'Home',
@@ -16,6 +60,7 @@ export default defineComponent({
 						dataSource={dataSource.value}
 						scroll={{ x: 1080 }}
 						pagination={{
+							size: 'large',
 							current: page.value,
 							pageSize: size.value,
 							total: total.value,
@@ -26,29 +71,14 @@ export default defineComponent({
 							title="媒体封面"
 							align="center"
 							width={125}
-							customRender={props => (
-								<Image
-									src={`${props.record.cover}?x-oss-process=style/resize`}
-									style={{ width: '96px', height: '54px', cursor: 'pointer' }}
-								></Image>
-							)}
+							customRender={props => <NodeCover node={props.record} />}
 						/>
 						<Table.Column title="媒体标题" width="18%" dataIndex="title" />
 						<Table.Column
 							title="媒体类型"
 							align="center"
 							width="7.5%"
-							customRender={props => {
-								return props.record.type === 1 ? (
-									<Tag color="red" style={{ margin: 0 }}>
-										单集媒体
-									</Tag>
-								) : (
-									<Tag color="cyan" style={{ margin: 0 }}>
-										多集媒体
-									</Tag>
-								)
-							}}
+							customRender={props => <NodeType node={props.record} />}
 						/>
 						<Table.Column title="媒体描述" dataIndex="description" />
 						<Table.Column title="排序号" align="center" width="7.5%" dataIndex="order" />
@@ -56,17 +86,7 @@ export default defineComponent({
 							title="媒体状态"
 							align="center"
 							width="7.5%"
-							customRender={props => {
-								return props.record.type === 1 ? (
-									<Tag style={{ margin: 0 }} color="green">
-										启用
-									</Tag>
-								) : (
-									<Tag style={{ margin: 0 }} color="pink">
-										禁用
-									</Tag>
-								)
-							}}
+							customRender={props => <NodeStatus node={props.record} />}
 						/>
 						<Table.Column title="创建时间" align="center" width="13.5%" dataIndex="createTime" />
 						<Table.Column title="操作" align="center" width="10%" />
